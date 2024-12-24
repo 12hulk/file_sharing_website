51887d4be266f7c21c5994a22408b88c4a1f217c
Dilpreet <dilpreetd88@gmail.com>
2024-12-24 15:01:10 -0600

    final

--- src/components/uploadForm.js
+++ src/components/uploadForm.js
@@ -77,6 +77,7 @@ const Upload = (email) => {
     };
 
     const handleDownload = (filename) => {
+        const fileName = filename;
         const supabase = createClient(
             "https://ekdoxzpypavhtoklntqv.supabase.co",
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZG94enB5cGF2aHRva2xudHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNzQ3NDAsImV4cCI6MjA0ODY1MDc0MH0.FyHH1ee-dfBThvAUeL4SaqCO6sJZzQ-2Scnnv-bInOA"
