7bc5f635816f5f5058452f00a4697a3aa9cc5b25
Dilpreet <dilpreetd88@gmail.com>
2024-12-28 12:35:13 -0600

    final

--- src/components/uploadForm.js
+++ src/components/uploadForm.js
@@ -34,6 +34,7 @@ const Upload = (userEmail) => {
                     headers: {
                         "Content-Type": "multipart/form-data", // Important for sending files
                         "file-name": file.name, // Send file name in headers (if needed)
+                        "userEmail": userEmail,
                     },
                 }
             );
