429ad72c4c97c9dd373e3b14cefac0a39ab18711
Dilpreet <dilpreetd88@gmail.com>
2024-12-30 11:51:08 -0600

    final

--- src/pages/uploadForm.js
+++ src/pages/uploadForm.js
@@ -52,23 +52,7 @@ const Upload = (userEmail) => {
                 console.log("File Name:", response.data.fileName);
                 console.log("Public URL:", response.data.fileUrl);
                 console.log("User Email:", response.data.userEmail);
-                const supabase = createClient(
-                    "https://ekdoxzpypavhtoklntqv.supabase.co",
-                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrZG94enB5cGF2aHRva2xudHF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNzQ3NDAsImV4cCI6MjA0ODY1MDc0MH0.FyHH1ee-dfBThvAUeL4SaqCO6sJZzQ-2Scnnv-bInOA"
-                );
-                // Insert file metadata into the 'files' table
-                const { error: dbError } = await supabase
-                    .from('files')
-                    .insert({
-                        user_email: response.data.userEmail,
-                        file_name: response.data.fileName,
-                        file_url: "fuckit",
-                        uploaded_at: new Date(),
-                    });
 
-                if (dbError) {
-                    return res.status(500).json({ error: "Error inserting file metadata" });
-                }
 
             } else {
                 setMessage("File upload failed. Please try again.");
